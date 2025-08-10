029d4b91c7165bf4bf35719e76ff45eda3ba2043
MarguJ <margulesjoa@gmail.com>
2025-08-10 01:13:59 -0300

    Pq no sirve de nada

--- Assets/Scripts2D/DungeonPathfinder2D.cs
+++ Assets/Scripts2D/DungeonPathfinder2D.cs
@@ -1,119 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 using BlueRaja;
+using UnityEngine;
 
-public class DungeonPathfinder2D {
-    public class Node {
-        public Vector2Int Position { get; private set; }
-        public Node Previous { get; set; }
-        public float Cost { get; set; }
+namespace Scripts2D
+{
+    public class DungeonPathfinder2D {
+        public class Node {
+            public Vector2Int Position { get; private set; }
+            public Node Previous { get; set; }
+            public float Cost { get; set; }
 
-        public Node(Vector2Int position) {
-            Position = position;
-        }
-    }
-
-    public struct PathCost {
-        public bool traversable;
-        public float cost;
-    }
-
-    static readonly Vector2Int[] neighbors = {
-        new Vector2Int(1, 0),
-        new Vector2Int(-1, 0),
-        new Vector2Int(0, 1),
-        new Vector2Int(0, -1),
-    };
-
-    Grid2D<Node> grid;
-    SimplePriorityQueue<Node, float> queue;
-    HashSet<Node> closed;
-    Stack<Vector2Int> stack;
-
-    public DungeonPathfinder2D(Vector2Int size) {
-        grid = new Grid2D<Node>(size, Vector2Int.zero);
-
-        queue = new SimplePriorityQueue<Node, float>();
-        closed = new HashSet<Node>();
-        stack = new Stack<Vector2Int>();
-
-        for (int x = 0; x < size.x; x++) {
-            for (int y = 0; y < size.y; y++) {
-                    grid[x, y] = new Node(new Vector2Int(x, y));
+            public Node(Vector2Int position) {
+                Position = position;
             }
         }
-    }
 
-    void ResetNodes() {
-        var size = grid.Size;
-
-        for (int x = 0; x < size.x; x++) {
-            for (int y = 0; y < size.y; y++) {
-                var node = grid[x, y];
-                node.Previous = null;
-                node.Cost = float.PositiveInfinity;
-            }
+        public struct PathCost {
+            public bool Traversable;
+            public float Cost;
         }
-    }
 
-    public List<Vector2Int> FindPath(Vector2Int start, Vector2Int end, Func<Node, Node, PathCost> costFunction) {
-        ResetNodes();
-        queue.Clear();
-        closed.Clear();
+        static readonly Vector2Int[] Neighbors = {
+            new Vector2Int(1, 0),
+            new Vector2Int(-1, 0),
+            new Vector2Int(0, 1),
+            new Vector2Int(0, -1),
+        };
 
-        queue = new SimplePriorityQueue<Node, float>();
-        closed = new HashSet<Node>();
+        readonly Grid2D<Node> _grid;
+        private SimplePriorityQueue<Node, float> _queue;
+        HashSet<Node> _closed;
+        readonly Stack<Vector2Int> _stack;
 
-        grid[start].Cost = 0;
-        queue.Enqueue(grid[start], 0);
+        public DungeonPathfinder2D(Vector2Int size) {
+            _grid = new Grid2D<Node>(size, Vector2Int.zero);
 
-        while (queue.Count > 0) {
-            Node node = queue.Dequeue();
-            closed.Add(node);
+            _queue = new SimplePriorityQueue<Node, float>();
+            _closed = new HashSet<Node>();
+            _stack = new Stack<Vector2Int>();
 
-            if (node.Position == end) {
-                return ReconstructPath(node);
-            }
-
-            foreach (var offset in neighbors) {
-                if (!grid.InBounds(node.Position + offset)) continue;
-                var neighbor = grid[node.Position + offset];
-                if (closed.Contains(neighbor)) continue;
-
-                var pathCost = costFunction(node, neighbor);
-                if (!pathCost.traversable) continue;
-
-                float newCost = node.Cost + pathCost.cost;
-
-                if (newCost < neighbor.Cost) {
-                    neighbor.Previous = node;
-                    neighbor.Cost = newCost;
-
-                    if (queue.TryGetPriority(node, out float existingPriority)) {
-                        queue.UpdatePriority(node, newCost);
-                    } else {
-                        queue.Enqueue(neighbor, neighbor.Cost);
-                    }
+            for (int x = 0; x < size.x; x++) {
+                for (int y = 0; y < size.y; y++) {
+                    _grid[x, y] = new Node(new Vector2Int(x, y));
                 }
             }
         }
 
-        return null;
-    }
-
-    List<Vector2Int> ReconstructPath(Node node) {
-        List<Vector2Int> result = new List<Vector2Int>();
-
-        while (node != null) {
-            stack.Push(node.Position);
-            node = node.Previous;
+        public DungeonPathfinder2D(HashSet<Node> closed)
+        {
+            _closed = closed;
         }
 
-        while (stack.Count > 0) {
-            result.Add(stack.Pop());
+        void ResetNodes() {
+            var size = _grid.Size;
+
+            for (int x = 0; x < size.x; x++) {
+                for (int y = 0; y < size.y; y++) {
+                    var node = _grid[x, y];
+                    node.Previous = null;
+                    node.Cost = float.PositiveInfinity;
+                }
+            }
         }
 
-        return result;
+        public List<Vector2Int> FindPath(Vector2Int start, Vector2Int end, Func<Node, Node, PathCost> costFunction) {
+            ResetNodes();
+            _queue.Clear();
+            _closed.Clear();
+
+            _queue = new SimplePriorityQueue<Node, float>();
+            _closed = new HashSet<Node>();
+
+            _grid[start].Cost = 0;
+            _queue.Enqueue(_grid[start], 0);
+
+            while (_queue.Count > 0) {
+                Node node = _queue.Dequeue();
+                _closed.Add(node);
+
+                if (node.Position == end) {
+                    return ReconstructPath(node);
+                }
+
+                foreach (var offset in Neighbors) {
+                    if (!_grid.InBounds(node.Position + offset)) continue;
+                    var neighbor = _grid[node.Position + offset];
+                    if (_closed.Contains(neighbor)) continue;
+
+                    var pathCost = costFunction(node, neighbor);
+                    if (!pathCost.Traversable) continue;
+
+                    float newCost = node.Cost + pathCost.Cost;
+
+                    if (newCost < neighbor.Cost) {
+                        neighbor.Previous = node;
+                        neighbor.Cost = newCost;
+
+                        if (_queue.TryGetPriority(node, out _)) {
+                            _queue.UpdatePriority(node, newCost);
+                        } else {
+                            _queue.Enqueue(neighbor, neighbor.Cost);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        List<Vector2Int> ReconstructPath(Node node) {
+            List<Vector2Int> result = new List<Vector2Int>();
+
+            while (node != null) {
+                _stack.Push(node.Position);
+                node = node.Previous;
+            }
+
+            while (_stack.Count > 0) {
+                result.Add(_stack.Pop());
+            }
+
+            return result;
+        }
     }
 }
